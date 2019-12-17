14bfc5d82b1a92a206bcc6e40d8b73aa360ea7b5
Kevin Downs <kevin.downs04@gmail.com>
2019-12-17 20:53:54 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -18,7 +18,10 @@ class InteractiveRecord
   end
   
   def.values_for_insert
-    
+    values = []
+    self.class.column_names.each do |col|
+      values << "'#{}'"
+    end
   end
   
   def self.table_name
