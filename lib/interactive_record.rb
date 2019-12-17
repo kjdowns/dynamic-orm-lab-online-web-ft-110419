cd20bd22208afcff7ed11eae4c68de96ea8658dd
Kevin Downs <kevin.downs04@gmail.com>
2019-12-17 21:04:01 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -26,8 +26,11 @@ class InteractiveRecord
   end
   
   def save
-    
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
+
   
   def self.table_name
     self.to_s.downcase.pluralize
